d4113192940b47e0fb27c900e1b2e8bccd32960f
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-10 10:44:29 +0530

    Deferring App Install Banner

--- Workspace/instagram-clone/public/src/js/app.js
+++ Workspace/instagram-clone/public/src/js/app.js
@@ -17,3 +17,21 @@ if ('serviceWorker' in navigator) {
 } else {
   console.log('Service worker is not supported!');
 }
+
+/*
+  Deferring Install Prompt: 
+  Starting with Chrome version 68, Chrome will not automatically shows an "App Install Banner". 
+  You instead have to listen to a 'beforeinstallprompt' event and then show the banner manually.
+  This event is fired by Chrome right before it's about to show that install banner.
+*/
+// 'beforeinstallprompt' event
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', (event) => {
+  console.log('beforeinstallprompt fired ...', event);
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+  // don't do anything.
+  return false;
+});
--- Workspace/instagram-clone/public/src/js/feed.js
+++ Workspace/instagram-clone/public/src/js/feed.js
@@ -4,6 +4,21 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  // deferredPrompt is set in app.js
+  if (deferredPrompt) {
+    // show the App install banner.
+    deferredPrompt.prompt();
+
+    // check user's choice
+    deferredPrompt.userChoice.then((choiceResult) => {
+      console.log(choiceResult);
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled the install.');
+      } else {
+        console.log('User added to home screen.');
+      }
+    });
+  }
 }
 
 function closeCreatePostModal() {
