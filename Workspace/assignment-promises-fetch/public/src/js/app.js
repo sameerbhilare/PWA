c788f7c5682c24a0905ff315a5fa004303592ff5
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-10 14:39:07 +0530

    Assignment: Promises and Fetch

---
+++ Workspace/assignment-promises-fetch/public/src/js/app.js
@@ -0,0 +1,79 @@
+// Handle the Promise "response" (=> the value you resolved) and return a fetch()
+// call to the value (= URL) you resolved (use a GET request)
+
+// Handle the response of the fetch() call and extract the JSON data, return that
+// and handle it in yet another then() block
+
+// Finally, output the "name" property of the data you got back (e.g. data.name) inside
+// the "output" element (see variables at top of the file)
+
+// Repeat the exercise with a PUT request you send to https://httpbin.org/put
+// Make sure to set the appropriate headers (as shown in the lecture)
+// Send any data of your choice, make sure to access it correctly when outputting it
+// Example: If you send {person: {name: 'Max', age: 28}}, you access data.json.person.name
+// to output the name (assuming your parsed JSON is stored in "data")
+
+// To finish the assignment, add an error to URL and add handle the error both as
+// a second argument to then() as well as via the alternative taught in the module
+
+var button = document.querySelector('#start-button');
+var output = document.querySelector('#output');
+
+button.addEventListener('click', function () {
+  // ==========================================
+  // FIRST ASSIGNMENT
+  // ==========================================
+
+  // Create a new Promise here and use setTimeout inside the function you pass to the constructor
+  var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      // <- Store this INSIDE the Promise you created!
+      // Resolve the following URL: https://reqres.in/api/users/2
+      resolve('https://reqres.in/api/users/2');
+    }, 2000);
+  })
+    .then(function (url) {
+      return fetch(url);
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      output.textContent = response.data.first_name + ' ' + response.data.last_name;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
+  // ==========================================
+  // SECOND ASSIGNMENT
+  // ==========================================
+  // Create a new Promise here and use setTimeout inside the function you pass to the constructor
+  /*
+  var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      // <- Store this INSIDE the Promise you created!
+      resolve('https://httpbin.org/puts');
+    }, 2000);
+  })
+    .then(function (url) {
+      return fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ person: { name: 'Max', age: 28 } }),
+      });
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      output.textContent = data.json.person.name;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+    */
+});
