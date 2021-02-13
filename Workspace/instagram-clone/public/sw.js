4f30463b40a0b546127e0e792fa23cc5993ba9bf
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-13 22:39:34 +0530

    Making route matching more precise

--- Workspace/instagram-clone/public/sw.js
+++ Workspace/instagram-clone/public/sw.js
@@ -131,12 +131,15 @@ self.addEventListener('activate', (event) => {
 });
 
 function isInArray(string, array) {
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] === string) {
-      return true;
-    }
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
   }
-  return false;
+  return array.indexOf(cachePath) > -1;
 }
 
 // ======================================================
