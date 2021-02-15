4f4070ecdd1799173c6877ad22adf39534d2ef8f
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-02-15 19:07:42 +0530

    Responsive Design - CSS and Media queries

--- Workspace/instagram-clone/public/src/js/feed.js
+++ Workspace/instagram-clone/public/src/js/feed.js
@@ -74,7 +74,7 @@ function createCard(data) {
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url("' + data.image + '")';
   cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
+  cardTitle.style.backgroundPosition = 'center'; // Or 'bottom'
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = 'white';
