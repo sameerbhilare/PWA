9bbb1ebca3430cda82adac1f508d93df72d2fd15
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-03-02 17:05:42 +0530

    Deleting unrequired project. To be replaced with PWAGram

--- Workspace/instagram-clone-node/backend/routes/subscription-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const subscriptionController = require('../controllers/subscription-controller');
-
-const router = express.Router();
-
-// POST - create a subscriptionController
-router.post('', subscriptionController.createSubscription);
-
-module.exports = router;
